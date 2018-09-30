1bb4c58aaf479f0f9fb795428977b9b3c5d9d5ac
Tony Lum <Tonyjlum@gmail.com>
2018-09-30 01:59:17 -0400

    basegame Simons.rb

--- W2D1 Simon/lib/simon.rb
+++ W2D1 Simon/lib/simon.rb
@@ -7,34 +7,61 @@ class Simon
     @sequence_length = 1
     @game_over = false
     @seq = []
+    #color stay for lest time on the screen as game progess.
+    @sleeptimer = 0.75
   end
 
   def play
-
+    until game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-    require_sequence
+
     show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      @sleeptimer -= 0.02
+      @sequence_length +=1
+    end
+
 
   end
 
   def show_sequence
+    add_random_color
+    @seq.each do |color|
+      puts color
 
+      sleep @sleeptimer
+      system("clear")
+      sleep 0.25
+    end
   end
 
   def require_sequence
-    
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+
   end
 
   def add_random_color
-    colors = ["red", "blue", "yellow", "green"]
-    seq << colors.sample
+    seq << COLORS.sample
+    seq
 
   end
 
   def round_success_message
-
+    "You got it!"
   end
 
   def game_over_message
